5940c4ebe19dbe69e0a36be78103902c9e0798bc
Nilton <niltonmaster159@gmail.com>
2026-09-01 21:00:50 -0500

    Modificar StudentEntity.java y StudentRepository.java

--- src/main/java/pe/edu/tecsup/lab03/Entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/Entities/StudentEntity.java
@@ -1,7 +1,15 @@
 package pe.edu.tecsup.lab03.Entities;
 
+import static java.lang.IO.print;
+
 public class StudentEntity {
     public String nombre;
     public String apellido;
     public int edad;
+
+
+
+    public static void metodo2(){
+        print("Inprimiendo resultados");
+    }
 }
--- src/main/java/pe/edu/tecsup/lab03/Repository/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/Repository/StudentRepository.java
@@ -1,7 +1,14 @@
 package pe.edu.tecsup.lab03.Repository;
 
+import static java.lang.IO.print;
+
 public class StudentRepository {
     String var1;
     String var2;
     int var3;
+
+
+    public static void metodo1(){
+        print("Prueba 1");
+    }
 }
